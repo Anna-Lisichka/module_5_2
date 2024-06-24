918eb7d8178c96ffe9094dcc22d1189dd3e5d90e
Anna-Lisichka <an_na22@mail.ru>
2024-06-24 22:50:25 +0300

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,10 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(self, floors):
+        self.numberOfFloors = floors
+        print(self.numberOfFloors)
+
+floor = House()
+floor.setNewNumberOfFloors(18)
